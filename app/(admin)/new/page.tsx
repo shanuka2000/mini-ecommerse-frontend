934c9c7551df854264e96673ada95c3a9acd2817
shanuka2000 <shanuka.inbox@gmail.com>
2023-10-07 15:38:26 +0530

    Updated folder structure

--- app/(admin)/new/page.tsx
+++ app/(admin)/new/page.tsx
@@ -1,11 +1,14 @@
 import Header from "@/components/header";
+import ProductForm from "@/components/product-form";
 import React from "react";
 
 export default function page() {
   return (
     <>
       <Header heading="Products" subHeading="Add new product" />
-      <main className=""></main>
+      <main className="">
+        <ProductForm />
+      </main>
     </>
   );
 }
---
+++ app/(root)/layout.tsx
@@ -0,0 +1,24 @@
+import "../globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Code 94 Assignment",
+  description: "This is a Next.js app, an assignment for Code 94 Labs",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} max-w-[90%] m-auto`}>
+        {children}
+      </body>
+    </html>
+  );
+}
