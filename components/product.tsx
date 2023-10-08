b2b64b4f21491d986d3e5e45c8c4c14c7f1b1755
shanuka2000 <shanuka.inbox@gmail.com>
2023-10-09 01:38:21 +0530

    Fixed issue in product component

--- components/product.tsx
+++ components/product.tsx
@@ -1,10 +1,12 @@
 import { ProductProps } from "@/lib/types";
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 import { AiFillDelete, AiFillStar } from "react-icons/ai";
 import { MdModeEdit } from "react-icons/md";
 
 export default function Product({
+  id,
   sku,
   image,
   product_name,
@@ -15,7 +17,7 @@ export default function Product({
       <td className="w-[20%] pt-10 pb-10 text-[#969191]">{sku}</td>
       <td className="w-[20%]">
         <Image
-          src={image}
+          src={image[0]}
           alt={product_name}
           quality={95}
           className="w-11 rounded-md"
@@ -25,7 +27,9 @@ export default function Product({
       <td className="w-[20%]">{price}</td>
       <td className="flex items-center justify-center gap-4 text-2xl pt-10 pb-10 text-[#001eb9]">
         <AiFillDelete />
-        <MdModeEdit />
+        <Link href={`/edit/${id}`}>
+          <MdModeEdit />
+        </Link>
         <AiFillStar />
       </td>
     </>
