21be37f355469d8b51fdb9d8881c49cec55348df
shanuka2000 <shanuka.inbox@gmail.com>
2023-10-09 01:37:37 +0530

    Added new dummy data

--- lib/data.ts
+++ lib/data.ts
@@ -8,48 +8,68 @@ export const dummyData = [
   {
     id: 0,
     sku: "#CA25",
-    image: product01,
+    image: [product01],
+    imageUrls: [
+      "https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1999&q=80",
+    ],
     product_name: "Product-Name",
     price: "$24.00",
+    qty: 24,
     product_description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
   },
   {
     id: 1,
     sku: "#CA34",
-    image: product02,
+    image: [product02],
+    imageUrls: [
+      "https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1999&q=80",
+    ],
     product_name: "Product-Name",
     price: "$24.00",
+    qty: 24,
     product_description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
   },
   {
     id: 2,
     sku: "#CA35",
-    image: product03,
+    image: [product03],
+    imageUrls: [
+      "https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1999&q=80",
+    ],
     product_name: "Product-Name",
     price: "$24.00",
+    qty: 24,
     product_description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
   },
   {
     id: 3,
     sku: "#CA56",
-    image: product04,
+    image: [product04],
+    imageUrls: [
+      "https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1999&q=80",
+    ],
     product_name: "Product-Name",
     price: "$24.00",
+    qty: 24,
     product_description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
   },
   {
     id: 4,
     sku: "#CA57",
-    image: product05,
+    image: [product05],
+    imageUrls: [
+      "https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1999&q=80",
+    ],
     product_name: "Product-Name",
     price: "$24.00",
+    qty: 24,
     product_description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
   },
-] as const;
+];
 
-export const tableHeadings = ["id", "sku", "image", "product name", "price"];
+export const tableHeadings = ["id", "sku", "product name", "price"];
