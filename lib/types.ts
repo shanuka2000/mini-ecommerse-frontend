c374df543c4f0c26b880d63553125d22588d3d09
shanuka2000 <shanuka.inbox@gmail.com>
2023-10-08 18:57:08 +0530

    Added new type

--- lib/types.ts
+++ lib/types.ts
@@ -1,2 +1,5 @@
+import { dummyData } from "./data";
+
 export type MainHeadingTypes = "Products" | "Favourite products";
 export type SubHeadingTypes = "Add new product" | "Edit product";
+export type ProductProps = (typeof dummyData)[number];
