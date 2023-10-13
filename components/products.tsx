b494cee44d447c09c312dd9554c9bfd11380440c
shanuka2000 <shanuka.inbox@gmail.com>
2023-10-13 22:27:47 +0530

    Implemented table loading animation

--- components/products.tsx
+++ components/products.tsx
@@ -4,6 +4,7 @@ import { dummyData, tableHeadings } from "@/lib/data";
 import React, { useEffect, useState } from "react";
 import Product from "./product";
 import { ProductItem } from "@/lib/types";
+import { TableLoader } from "./loaders";
 
 export default function Products() {
   const [products, setProducts] = useState<ProductItem[]>([]);
@@ -21,29 +22,35 @@ export default function Products() {
   }, []);
 
   return (
-    <table className="flex-1">
-      <thead className="">
-        <tr className="text-left">
-          {tableHeadings.map((item, index) => (
-            <th className="text-[#001eb9] uppercase" key={index}>
-              {item}
-            </th>
-          ))}
-          <th></th>
-        </tr>
-      </thead>
-      <tbody className="">
-        <>
-          {products.map((productItem) => (
-            <tr
-              className="border-b-2 border-b-[#969191]/20 last:border-b-0"
-              key={productItem.id}
-            >
-              <Product {...productItem} />
+    <>
+      {products.length > 0 ? (
+        <table className="flex-1">
+          <thead className="">
+            <tr className="text-left">
+              {tableHeadings.map((item, index) => (
+                <th className="text-[#001eb9] uppercase" key={index}>
+                  {item}
+                </th>
+              ))}
+              <th></th>
             </tr>
-          ))}
-        </>
-      </tbody>
-    </table>
+          </thead>
+          <tbody className="">
+            <>
+              {products.map((productItem) => (
+                <tr
+                  className="border-b-2 border-b-[#969191]/20 last:border-b-0"
+                  key={productItem.id}
+                >
+                  <Product {...productItem} />
+                </tr>
+              ))}
+            </>
+          </tbody>
+        </table>
+      ) : (
+        <TableLoader />
+      )}
+    </>
   );
 }
