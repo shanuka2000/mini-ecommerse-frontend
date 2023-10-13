8feff55c71f279490c7e8f2a8563786ea3c654e7
shanuka2000 <shanuka.inbox@gmail.com>
2023-10-13 22:26:47 +0530

    Added product data fetch and form loading animation

--- app/(admin)/edit/[id]/page.tsx
+++ app/(admin)/edit/[id]/page.tsx
@@ -1,23 +1,48 @@
 "use client";
 
 import Header from "@/components/header";
+import { FormLoader } from "@/components/loaders";
 import ProductForm from "@/components/product-form";
-import { dummyData } from "@/lib/data";
+import { ProductItem } from "@/lib/types";
+import { useEffect, useState } from "react";
 
 type Props = {
   params: {
-    id: (typeof dummyData)[number]["id"];
+    id: string;
   };
 };
 
 export default function EditPage({ params: { id } }: Props) {
-  const data = dummyData[id];
+  const [data, setData] = useState<ProductItem>();
+
+  useEffect(() => {
+    const fetchedProducts = async () => {
+      const apiUrl = process.env.NEXT_PUBLIC_API_BASE_URL as string;
+      const additionalPath = `/${id}`;
+      const response = await fetch(apiUrl + additionalPath);
+      const data: ProductItem = await response.json();
+      setData(data);
+    };
+
+    fetchedProducts();
+  }, []);
 
   return (
     <>
       <Header heading="Products" subHeading="Edit product" />
       <main className="w-[90%] m-auto">
-        <ProductForm {...data} />
+        {data ? (
+          <ProductForm
+            id={data?.id as string}
+            sku={data?.sku as string}
+            name={data?.name as string}
+            qty={data?.qty as number}
+            description={data?.description as string}
+            images={data?.images as string[]}
+          />
+        ) : (
+          <FormLoader />
+        )}
       </main>
     </>
   );
